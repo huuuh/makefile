81a3dc4920a841f90f067962904a5c4751cf415a
huuuh <hljhuu@qq.com>
2018-03-05 19:07:03 +0800

    这是一个mkaefile文件的实验测试程序

---
+++ 1_test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+//c的网络的编程
+int main(int argc,char *argv)
+{
+
+    printf("hello world\n");
+    return 0;
+}
---
+++ Makefile
@@ -0,0 +1,5 @@
+work:hello
+	./hello
+hello:1_test.c
+	gcc -o hello 1_test.c
+
